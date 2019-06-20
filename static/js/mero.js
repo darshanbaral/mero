8247ef5a1b869f80179817da1e8789fcafb253d4
Darshan Baral <darshanbaral@gmail.com>
2019-06-19 20:56:35 -0700

    js to replace css hover menu because iOS

--- static/js/mero.js
+++ static/js/mero.js
@@ -6,3 +6,27 @@ setColors = function() {
   imagecaption.forEach(elem => elem.classList.add("initialism"));
 };
 window.onload = setColors;
+
+let toggleMenu = function(x) {
+  let elem = x.nextElementSibling;
+  if (elem.classList.contains("d-none")) {
+    elem.classList.remove("d-none");
+  } else {
+    elem.classList.add("d-none");
+  }
+};
+let checkClick = function() {
+  let menuContainer = document.querySelector(".dropdown-content");
+  if (
+    !menuContainer.contains(event.target) &&
+    !document.querySelector(".dropdown").contains(event.target)
+  ) {
+    menuContainer.classList.add("d-none");
+  }
+};
+window.onclick = function() {
+  checkClick();
+};
+window.ontouchstart = function() {
+  checkClick();
+};
